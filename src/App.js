abd58d435e2ab60d4783671952c7fcf6c1de745e
palmaoff <fedorin.anton@mail.ru>
2020-05-10 17:31:06 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './App.css'
 import Header from './components/Header'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+const App = () => {
   return (
     <div>
       <Header />
     </div>
-  );
+  )
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { Container, Navbar, Dropdown, DropdownButton } from 'react-bootstrap'
+import { Button } from 'react-bootstrap';
 
 const Header = () => {
-  return (
-    <div className="header">
-      To Do List
-    </div>
-  );
+	return (
+		<Navbar CollapseOnSelect expand="md" bg="dark" variant="dark">
+			<Container>
+				<div className="navbar-text">
+					<h5>To Do List</h5>
+   				</div>
+				<Button variant="primary">new task</Button>
+			</Container>
+		</Navbar>
+	)
 }
 
-export default Header;
+export default Header
